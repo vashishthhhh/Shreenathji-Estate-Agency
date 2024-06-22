2efcb0c967fdeff93d8f93d62fda80b6cb7984f2
vashishthhhh <120786584+vashishthhhh@users.noreply.github.com>
2024-06-22 14:03:04 +0530

    initial comit
    initial commit

---
+++ js/main.js
@@ -0,0 +1,104 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top');
+        } else {
+            $('.nav-bar').removeClass('sticky-top');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 24,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
+
+// get appointment criteriia //
+
+    const validCountryCodes = ["1", "44", "91", "81", "61", "33", "49", "86", "39"]; // Add more valid country codes as needed
+
+    document.getElementById('tel').addEventListener('input', function () {
+        const input = this.value;
+        const errorSpan = document.getElementById('tel-error');
+        const regex = /^\+(\d{1,3})(\d{6,14})$/;
+        const match = input.match(regex);
+        
+        if (match) {
+            const countryCode = match[1];
+            if (validCountryCodes.includes(countryCode)) {
+                errorSpan.textContent = '';
+            } else {
+                errorSpan.textContent = 'Invalid country code.';
+            }
+        } else {
+            errorSpan.textContent = 'Invalid phone number format.';
+        }
+    });
+
+
